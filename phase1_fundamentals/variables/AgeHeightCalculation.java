d6a731d0bd09fe3d3753d8bd14ac9e942f5d90c6
brunombs <brunozs@msn.com>
2024-11-09 00:07:21 -0300

    first one :)

---
+++ phase1_fundamentals/variables/AgeHeightCalculation.java
@@ -0,0 +1,12 @@
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter your age: ");
+    int age = input.nextInt();
+
+    System.out.print("Enter your height in cm: ");
+    int height = input.nextInt();
+
+    System.out.println("Your age is " + age);
+    System.out.println("Your height is " + height + "cm");
+}
