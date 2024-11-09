ec2966f0e0bdcb165d1a7f19f40c792b3831fb65
brunombs <brunozs@msn.com>
2024-11-09 00:29:35 -0300

    improving exercise

--- phase1_fundamentals/variables/TemperatureConverter.java
+++ phase1_fundamentals/variables/TemperatureConverter.java
@@ -1,12 +1,8 @@
-import java.util.Scanner;
-
-public class TemperatureConverter {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a temperature in Fahrenheit: ");
-        double fahrenheit = input.nextDouble();
-        double celsius = (fahrenheit - 32) * 5 / 9;
-        System.out.println(fahrenheit + " fahrenheit in celsius is " + celsius);
-        input.close();
-    }
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a temperature in Fahrenheit: ");
+    double fahrenheit = input.nextDouble();
+    double celsius = (fahrenheit - 32) * 5 / 9;
+    System.out.println(fahrenheit + " fahrenheit in celsius is " + celsius);
+    input.close();
 }
