f720273db7dbebd9b2d4f2affe3d01d74d3345c4
brunombs <brunozs@msn.com>
2024-11-09 00:29:05 -0300

    improving exercise

--- phase1_fundamentals/variables/AgeHeightCalculation.java
+++ phase1_fundamentals/variables/AgeHeightCalculation.java
@@ -1,18 +1,14 @@
-import java.util.Scanner;
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
-public class AgeHeightCalculation {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    System.out.print("Enter your age: ");
+    int age = input.nextInt();
 
-        System.out.print("Enter your age: ");
-        int age = input.nextInt();
+    System.out.print("Enter your height in cm: ");
+    int height = input.nextInt();
 
-        System.out.print("Enter your height in cm: ");
-        int height = input.nextInt();
+    System.out.println("Your age is " + age);
+    System.out.println("Your height is " + height + "cm");
 
-        System.out.println("Your age is " + age);
-        System.out.println("Your height is " + height + "cm");
-
-        input.close();
-    }
+    input.close();
 }
