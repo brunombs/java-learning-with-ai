8448891c13d464977b0340b0d4a732cf05bfad2d
brunombs <brunozs@msn.com>
2024-11-09 00:30:00 -0300

    improving exercise

--- phase1_fundamentals/variables/DrivingEligibility.java
+++ phase1_fundamentals/variables/DrivingEligibility.java
@@ -1,25 +1,26 @@
-import java.util.Scanner;
-
 public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     boolean olderThan18;
     boolean hasDriverLicense;
 
     System.out.println("Welcome to the Driving Eligibility!");
-    System.out.println("Do you have a driver license? (true/false)");
+    System.out.print("Do you have a driver license? (true/false): ");
     hasDriverLicense = input.nextBoolean();
+
     if (!hasDriverLicense) {
         System.out.println("You can't drive");
+        input.close();
         return;
     }
 
-    System.out.println("Are you older than 18? (true/false)");
+    System.out.print("Are you older than 18? (true/false): ");
     olderThan18 = input.nextBoolean();
 
-    if (olderThan18 && hasDriverLicense) {
+    if (olderThan18) {
         System.out.println("You are eligible to drive");
-    }
-    else {
+    } else {
         System.out.println("You are not eligible to drive");
     }
+
+    input.close();
 }
