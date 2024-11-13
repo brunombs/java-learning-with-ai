ced46276dce5bb46668f5e4d1b1b5c015e6674f2
brunombs <brunozs@msn.com>
2024-11-13 16:32:44 -0300

    adding exercise

---
+++ phase2_oop/05_abstract_classes/shape_abstract/Main.java
@@ -0,0 +1,8 @@
+package shape_abstract;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(10, 5);
+        System.out.println("Rectangle area: " + rectangle.calculateArea());
+    }
+}
---
+++ phase2_oop/05_abstract_classes/shape_abstract/ShapeAbstract.java
@@ -0,0 +1,5 @@
+package shape_abstract;
+
+abstract class ShapeAbstract {
+    abstract double calculateArea();
+}
