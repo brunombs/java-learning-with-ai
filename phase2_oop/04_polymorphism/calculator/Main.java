621e814fa6025666c347651124c3b7144617ff68
brunombs <brunozs@msn.com>
2024-11-13 14:04:26 -0300

    adding exercise

---
+++ phase2_oop/04_polymorphism/calculator/Main.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        calculator calc = new calculator();
+
+        System.out.println(calc.add(5,7));
+        System.out.println(calc.subtract(52,14));
+        System.out.println(calc.add(9,23, 54));
+    }
+}
---
+++ phase2_oop/04_polymorphism/calculator/calculator.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class calculator {
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int add(int a, int b, int c){
+        return a + b + c;
+    }
+
+    public int subtract(int a, int b){
+        return a - b;
+    }
+}
