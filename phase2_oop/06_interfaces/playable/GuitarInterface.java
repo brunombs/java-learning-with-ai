6ec45b778ccb740c9312720f3e756a7bf7fea26a
brunombs <brunozs@msn.com>
2024-11-13 18:02:37 -0300

    improving exercise

--- phase2_oop/06_interfaces/playable/GuitarInterface.java
+++ phase2_oop/06_interfaces/playable/GuitarInterface.java
@@ -1,13 +1,39 @@
 package playable;
 
 public class GuitarInterface implements Playable {
+    private int volume;
+
+    public GuitarInterface(int volume) {
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+        System.out.println("Volume set to: " + this.volume);
+    }
+
+    void increaseVolume() {
+        System.out.println("Current volume: " + volume);
+        this.volume++;
+        System.out.println("Volume increased. New volume: " + volume);
+    }
+
+    void decreaseVolume() {
+        this.volume--;
+        System.out.println("Volume decreased. New volume: " + volume);
+    }
+
     @Override
     public void play() {
-        System.out.println("Playing guitar sounds");
+        System.out.println("Playing guitar sounds...");
     }
 
     @Override
     public void stop() {
-        System.out.println("Stopping guitar sounds");
+        System.out.println("Stopping guitar sounds...");
     }
 }
--- phase2_oop/06_interfaces/playable/Main.java
+++ phase2_oop/06_interfaces/playable/Main.java
@@ -2,9 +2,13 @@ package playable;
 
 public class Main {
     public static void main(String[] args) {
-        GuitarInterface guitarInterface = new GuitarInterface();
+        GuitarInterface guitarInterface = new GuitarInterface(5);
         PianoInterface pianoInterface = new PianoInterface();
 
+        System.out.println("Current volume: " + guitarInterface.getVolume());
+        guitarInterface.setVolume(8);
+        guitarInterface.increaseVolume();
+        guitarInterface.decreaseVolume();
         guitarInterface.play();
         guitarInterface.stop();
         pianoInterface.play();
