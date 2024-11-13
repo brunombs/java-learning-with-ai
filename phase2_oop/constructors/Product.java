9d38063d09d77664f7bb604e93fda5f694244b88
brunombs <brunozs@msn.com>
2024-11-12 22:57:43 -0300

    adding exercise

---
+++ phase2_oop/constructors/Product.java
@@ -0,0 +1,23 @@
+public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double totalValue(){
+        return price * quantity;
+    }
+
+    public static void main(String[] args) {
+        Product computer = new Product("Computer", 1500, 10);
+        Product bicycle = new Product("Bicycle", 550.99, 30);
+
+        System.out.println(computer.totalValue());
+        System.out.println(bicycle.totalValue());
+    }
+}
