990a625e2c07aa683e7734bf9f4749bd702e6dea
brunombs <brunozs@msn.com>
2024-11-12 23:40:52 -0300

    adding exercise

--- phase2_oop/02_encapsulation/BankAccountv3.java
+++ phase2_oop/02_encapsulation/BankAccountv3.java
@@ -10,19 +10,36 @@ public class BankAccountv3 {
     }
 
     public void deposit(double amount) {
-        balance = balance + amount;
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful! Current balance: " + balance);
+        } else {
+            System.out.println("Invalid deposit amount! The amount must be positive.");
+        }
     }
 
     public void withdraw(double amount) {
-        balance = balance - amount;
+        if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount! The amount must be positive.");
+        } else if (amount > balance) {
+            System.out.println("Insufficient funds! Unable to withdraw this amount: " + amount);
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal of " + amount + " successful! Current balance: " + balance);
+        }
     }
 
     public static void main(String[] args) {
         BankAccountv3 account = new BankAccountv3();
-        System.out.println(account.getBalance());
+        account.setBalance(100);
+
+        System.out.println("Initial balance: " + account.getBalance());
+
         account.deposit(50);
-        System.out.println(account.getBalance());
-        account.withdraw(23);
-        System.out.println(account.getBalance());
+        account.deposit(-10);
+
+        account.withdraw(30);
+        account.withdraw(150);
+        account.withdraw(-20);
     }
 }
