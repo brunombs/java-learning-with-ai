65de170a33324f7cb8f662c9c9db924e8f1f7cd7
brunombs <brunozs@msn.com>
2024-11-27 05:04:46 -0300

    adding exercise

--- phase3_collections/00_arraylist/StudentList.java
+++ phase3_collections/00_arraylist/StudentList.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+
 public class StudentList {
+    static class Student {
+        String name;
+        int age;
+
+        public Student(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        void printDetails() {
+            System.out.println("Name: " + name + ", age: " + age);
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Bruno", 27));
+        students.add(new Student("John", 24));
+        students.add(new Student("Jane", 31));
+
+        for (Student student : students) {
+            student.printDetails();
+        }
+
+        students.remove(students.get(1));
+        System.out.println("Students in the list: " + students.size());
+
+        for (Student student : students) {
+            student.printDetails();
+        }
+    }
 }
+
+
