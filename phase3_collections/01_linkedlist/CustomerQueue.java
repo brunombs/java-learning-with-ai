2c4dc547bf3ade457f3ee836998e1fa56f098ccc
brunombs <brunozs@msn.com>
2024-11-29 05:21:23 -0300

    adding exercise

--- phase3_collections/01_linkedlist/CustomerQueue.java
+++ phase3_collections/01_linkedlist/CustomerQueue.java
@@ -1,2 +1,28 @@
+import java.util.LinkedList;
+
 public class CustomerQueue {
+    LinkedList<String> onQueue;
+
+    public CustomerQueue(LinkedList<String> onQueue) {
+        this.onQueue = onQueue;
+    }
+
+    void serveCustomer(){
+        System.out.println("Customer served: " + onQueue.getFirst());
+        onQueue.pop();
+    }
+
+    public static void main(String[] args) {
+        LinkedList<String> onQueue = new LinkedList<>();
+        CustomerQueue customerQueue = new CustomerQueue(onQueue);
+        onQueue.add("A");
+        onQueue.add("B");
+        onQueue.add("C");
+        System.out.println("Remaining customers: " + onQueue);
+        customerQueue.serveCustomer();
+        System.out.println("Remaining customers: " + onQueue);
+        customerQueue.serveCustomer();
+        System.out.println("Remaining customers: " + onQueue);
+        customerQueue.serveCustomer();
+    }
 }
