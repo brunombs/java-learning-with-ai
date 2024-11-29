507ca3dae7483772fc0976a8d6b1ee581300e4ca
brunombs <brunozs@msn.com>
2024-11-29 05:44:36 -0300

    improving exercise

--- phase3_collections/01_linkedlist/TaskManager.java
+++ phase3_collections/01_linkedlist/TaskManager.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class TaskManager {
     LinkedList<String> tasks;
@@ -7,10 +8,9 @@ public class TaskManager {
         this.tasks = tasks;
     }
 
-    public void markTaskAsCompleted(){
-        System.out.println("Task " + tasks.getFirst() + " marked as completed");
-        System.out.println(tasks.size() + " pending tasks ");
-        tasks.removeFirst();
+    public void markTaskAsCompleted(int num){
+        System.out.println("Task " + tasks.get(num -1) + " marked as completed");
+        tasks.remove(num -1);
     }
 
     public void viewTasks(){
@@ -18,15 +18,25 @@ public class TaskManager {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         LinkedList<String> tasks = new LinkedList<>();
         TaskManager myTasks = new TaskManager(tasks);
         tasks.add("Study Java");
         tasks.add("Practise Java Coding");
         tasks.add("Review LinkedList");
-        myTasks.viewTasks();
-        myTasks.markTaskAsCompleted();
         tasks.addFirst("Sleep for at least 8 hours");
+        System.out.println("Which task do you want to mark as completed?: ");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+        int markAsCompleted = scanner.nextInt();
+        myTasks.markTaskAsCompleted(markAsCompleted);
+        System.out.println("Which task do you want to mark as completed?: ");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+        markAsCompleted = scanner.nextInt();
+        myTasks.markTaskAsCompleted(markAsCompleted);
         myTasks.viewTasks();
-
     }
 }
